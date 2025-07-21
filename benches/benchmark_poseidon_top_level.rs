10ea2581b91bfe8915426beae5c13a10729fc591
b-wagn <crypto@benedikt-wagner.dev>
2025-07-21 08:03:32 +0200

    Add new instantiations for top level

--- benches/benchmark_poseidon_top_level.rs
+++ benches/benchmark_poseidon_top_level.rs
@@ -3,10 +3,7 @@ use rand::{thread_rng, Rng};
 
 use hashsig::{
     signature::{
-        generalized_xmss::instantiations_poseidon_top_level::{
-            lifetime_2_to_the_18::SIGTopLevelTargetSumLifetime18Dim40Base12,
-            lifetime_2_to_the_26::SIGTopLevelTargetSumLifetime26Dim64Base8,
-        },
+        generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_18::SIGTopLevelTargetSumLifetime18Dim64Base8,
         SignatureScheme,
     },
     MESSAGE_LENGTH,
@@ -88,14 +85,8 @@ pub fn benchmark_signature_scheme<S: SignatureScheme>(c: &mut Criterion, descrip
 
 pub fn bench_function_poseidon_top_level(c: &mut Criterion) {
     // benchmarking lifetime 2^18
-    benchmark_signature_scheme::<SIGTopLevelTargetSumLifetime18Dim40Base12>(
+    benchmark_signature_scheme::<SIGTopLevelTargetSumLifetime18Dim64Base8>(
         c,
-        "Top Level Target Sum, Lifetime 2^18, Dimension 40, Base 12",
-    );
-
-    // benchmarking lifetime 2^26
-    benchmark_signature_scheme::<SIGTopLevelTargetSumLifetime26Dim64Base8>(
-        c,
-        "Top Level Target Sum, Lifetime 2^26, Dimension 64, Base 8",
+        "Top Level Target Sum, Lifetime 2^18, Dimension 64, Base 8",
     );
 }
