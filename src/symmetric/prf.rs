db9b69f802f12dd1c0b445601ccb0f96831ab35c
b-wagn <crypto@benedikt-wagner.dev>
2025-07-28 14:52:49 +0200

    Fix doc comments for PRF

--- src/symmetric/prf.rs
+++ src/symmetric/prf.rs
@@ -1,15 +1,15 @@
 use rand::Rng;
 use serde::{Serialize, de::DeserializeOwned};
 
-/// Trait to model a pseudorandom function
+/// Trait to model a pseudorandom function (PRF)
 pub trait Pseudorandom {
     type Key: Send + Sync + Serialize + DeserializeOwned;
     type Output;
 
-    /// Sample a random domain element
+    /// Sample a random key for the PRF
     fn key_gen<R: Rng>(rng: &mut R) -> Self::Key;
 
-    /// Apply the one-way function to an epoch and an index
+    /// Apply the PRF to an epoch and an index
     fn apply(key: &Self::Key, epoch: u32, index: u64) -> Self::Output;
 
     /// Function to check internal consistency of any given parameters
